012400459725e91b636c9c8b2210e9c2540c11c3
tilankamihingu <tilankamihingu2018@gmail.com>
2021-11-29 12:59:19 +0530

    start

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import { Button, Menu, Typography, Avatar } from 'antd';
+import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
+import icon from '../images/cryptocurrency.png';
+const Navbar = () => {
+    return (
+        <div className="nav-container">
+            <div className="logo-container">
+                <Avatar src={icon} size="large" />
+                <Typography.Title level={2} className="logo">
+                    <Link to="/">Cryp</Link>
+                </Typography.Title>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
