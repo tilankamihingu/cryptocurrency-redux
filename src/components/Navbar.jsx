37db4794f4039b2adb8971bfaaad08a81cafdbea
tilankamihingu <tilankamihingu2018@gmail.com>
2021-12-14 20:38:32 +0530

    nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,6 +12,20 @@ const Navbar = () => {
                     <Link to="/">Cryp</Link>
                 </Typography.Title>
             </div>
+            <Menu theme="dark">
+                <Menu.Item icon={<HomeOutlined />}>
+                    <Link to="/">Home</Link>
+                </Menu.Item>
+                <Menu.Item icon={<FundOutlined />}>
+                    <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+                </Menu.Item>
+                <Menu.Item icon={<MoneyCollectOutlined />}>
+                    <Link to="/exchanges">Exchanges</Link>
+                </Menu.Item>
+                <Menu.Item icon={<BulbOutlined />}>
+                    <Link to="/">News</Link>
+                </Menu.Item>
+            </Menu>
         </div>
     )
 }
